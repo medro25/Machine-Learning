6a28cfe209f8035d5984c8dfad9d1717cb4442c4
medro25 <58664810+medro25@users.noreply.github.com>
2022-01-14 12:57:28 +0200

    Add files via upload
    Linear regression algorithm

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
+import matplotlib.pyplot as pyplot
 from sklearn.utils import shuffle
+import pickle
+import os
+from matplotlib import style
 # read the data using pandas library
 data =pd.read_csv("student-mat.csv", sep=";")
 # categorize data
@@ -16,15 +20,31 @@ predict="G3"
 x= np.array(data.drop([predict], 1))
 # set up the labels in an array
 y=np.array(data[predict])
-# dividing the data into test and train data to test our accuracy of our model
-x_train,x_test,y_train,y_test= sklearn.model_selection.train_test_split(x,y, test_size=0.1)
-#implementing our training  model ( algorithm)
-linear=linear_model.LinearRegression()
-#find the best fit to the data using X train data and y train data
-linear.fit(x_train,y_train)
-# test our model : how well is working
-acc=linear.score(x_test,y_test)
-print(acc)
+ # let´s try to find best result with good accuracy
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+"""""
+best= 0
+for _ in range (30):
+    # dividing the data into test and train data to test our accuracy of our model
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+    # implementing our training  model ( algorithm)
+    linear = linear_model.LinearRegression()
+    # find the best fit to the data using X train data and y train data
+    linear.fit(x_train, y_train)
+    # test our model : how well is working
+    acc = linear.score(x_test, y_test)
+    print(acc)
+    if acc > best:
+        best=acc
+# save the model to avoid training the model every time
+    with open(" studentmodel.pickle","wb") as f:
+         pickle.dump(linear,f)"""""
+    #read the pickle  file
+filename = " studentmodel.pickle"
+amine= os.stat(filename).st_size
+if amine>0:
+    pickle_in= open(filename,"rb")
+    linear=pickle.load(pickle_in)
 # print out the coefficiant and the intercept
 # we will get 5 coefficiant since we have 5 attributes ( 5 dimentions )
 print("coeff: \n",linear.coef_)
@@ -33,3 +53,10 @@ prediction =linear.predict(x_test)
 #print("prediction:\n",prediction)
 for x in range (len(prediction)):
     print((prediction[x],x_test[x],y_test[x]))
+
+    d= "G1"
+    style.use("ggplot")
+    pyplot.scatter(data[d],data["G3"])
+    pyplot.xlabel(d)
+    pyplot.ylabel("G3")
+    pyplot.show()
